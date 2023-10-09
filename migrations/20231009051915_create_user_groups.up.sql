6b8552615eb58454b42035457be399d99263a0d3
Vladimir Zarypov <krre31@gmail.com>
2023-10-09 17:04:07 +0300

    Add creating users table into migration

--- migrations/20231009051915_create_user_groups.up.sql
+++ migrations/20231009051915_create_user_groups.up.sql
@@ -5,5 +5,18 @@ CREATE TABLE IF NOT EXISTS user_groups (
     created_ts timestamptz NOT NULL DEFAULT now()
 );
 
-INSERT INTO user_groups(name, role) VALUES('Admins', 0);
-INSERT INTO user_groups(name, role) VALUES('Users', 1);
+INSERT INTO user_groups (name, role) VALUES('Admins', 0);
+INSERT INTO user_groups (name, role) VALUES('Users', 1);
+
+CREATE TABLE IF NOT EXISTS users (
+    id serial PRIMARY KEY,
+    group_id INT NOT NULL DEFAULT 1 REFERENCES user_groups(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    token TEXT NOT NULL UNIQUE,
+    created_ts timestamptz NOT NULL DEFAULT now(),
+    updated_ts timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE INDEX email_idx ON users (email);
+CREATE INDEX token_idx ON users (token);

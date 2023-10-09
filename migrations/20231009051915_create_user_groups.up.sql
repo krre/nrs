49d532431435c1655cab728a79cf762a001c29ac
Vladimir Zarypov <krre31@gmail.com>
2023-10-09 16:46:43 +0300

    Append inserting default rows into migration of creating user groups

--- migrations/20231009051915_create_user_groups.up.sql
+++ migrations/20231009051915_create_user_groups.up.sql
@@ -4,3 +4,6 @@ CREATE TABLE IF NOT EXISTS user_groups (
     role smallint NOT NULL,
     created_ts timestamptz NOT NULL DEFAULT now()
 );
+
+INSERT INTO user_groups(name, role) VALUES('Admins', 0);
+INSERT INTO user_groups(name, role) VALUES('Users', 1);

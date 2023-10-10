bb6314be022065576abd4e0ffd96c9699e9b8045
Vladimir Zarypov <krre31@gmail.com>
2023-10-10 16:25:16 +0300

    Fix Postgres date names

--- migrations/20231009051915_create_user_groups.up.sql
+++ migrations/20231009051915_create_user_groups.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS user_groups (
     id serial PRIMARY KEY,
     name TEXT NOT NULL,
     role smallint NOT NULL,
-    created_ts timestamptz NOT NULL DEFAULT now()
+    created_at timestamptz NOT NULL DEFAULT now()
 );
 
 INSERT INTO user_groups (name, role) VALUES('Admins', 0);
@@ -14,8 +14,8 @@ CREATE TABLE IF NOT EXISTS users (
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     token TEXT NOT NULL UNIQUE,
-    created_ts timestamptz NOT NULL DEFAULT now(),
-    updated_ts timestamptz NOT NULL DEFAULT now()
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now()
 );
 
 CREATE INDEX users_email_idx ON users (email);

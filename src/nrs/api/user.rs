08e36bc1b70f24bbb592ad3cb1fd695c9e7e7fcf
Vladimir Zarypov <krre31@gmail.com>
2023-10-15 14:46:16 +0300

    Add creating user

--- src/nrs/api/user.rs
+++ src/nrs/api/user.rs
@@ -1,14 +1,41 @@
-use axum::extract;
+use axum::{
+    extract::{self, State},
+    http::StatusCode,
+    response::IntoResponse,
+};
 
 use serde::Deserialize;
+use sqlx::PgPool;
 
 #[derive(Deserialize)]
 pub struct CreateUser {
+    sign: String,
     name: String,
     email: String,
     password: String,
 }
 
-pub async fn create_user(extract::Json(payload): extract::Json<CreateUser>) {
-    println!("{} {} {}", payload.name, payload.email, payload.password);
+struct User {
+    id: i32,
+}
+
+pub async fn create_user(
+    State(pool): State<PgPool>,
+    extract::Json(payload): extract::Json<CreateUser>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let user = sqlx::query_as!(
+        User,
+        "INSERT INTO users (sign, name, email, password) values ($1, $2, $3, $4) RETURNING id",
+        payload.sign,
+        payload.name,
+        payload.email,
+        payload.password
+    )
+    .fetch_one(&pool)
+    .await
+    .unwrap();
+
+    println!("id {}", user.id);
+
+    Ok(StatusCode::CREATED)
 }

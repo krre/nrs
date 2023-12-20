2ac54369ee9c25633c27d87b866dcf7d39700d0f
Vladimir Zarypov <krre31@gmail.com>
2023-12-20 07:14:36 +0200

    Add `POST project` handler

--- src/nrs/api/endpoint/project.rs
+++ src/nrs/api/endpoint/project.rs
@@ -1,5 +1,5 @@
 pub(crate) mod router {
-    use axum::routing::{self, get};
+    use axum::routing::{self, get, post};
     use sqlx::{Pool, Postgres};
 
     use super::handler;
@@ -7,6 +7,7 @@ pub(crate) mod router {
     pub fn new(pool: &Pool<Postgres>) -> routing::Router {
         routing::Router::new()
             .route("/", get(handler::get))
+            .route("/", post(handler::create))
             .with_state(pool.clone())
     }
 }
@@ -15,13 +16,29 @@ mod handler {
     use axum::{extract::State, Json};
     use sqlx::PgPool;
 
-    use crate::api::extract::AuthUser;
+    use crate::api::extract::{AuthUser, ValidPayload};
     use crate::api::Result;
 
-    mod request {}
+    mod request {
+        use serde::Deserialize;
+        use validator::Validate;
+
+        #[derive(Deserialize, Validate)]
+        pub struct Create {
+            #[validate(length(min = 1))]
+            pub name: String,
+            pub template: i16,
+            pub description: String,
+        }
+    }
 
     mod response {
         use serde::Serialize;
+
+        #[derive(Serialize)]
+        pub struct Create {
+            pub id: i32,
+        }
 
         #[derive(Serialize)]
         pub struct Project {
@@ -32,6 +49,29 @@ mod handler {
             pub created_at: chrono::DateTime<chrono::Utc>,
             pub updated_at: chrono::DateTime<chrono::Utc>,
         }
+    }
+
+    pub async fn create(
+        State(pool): State<PgPool>,
+        AuthUser(user_id): AuthUser,
+        ValidPayload(payload): ValidPayload<request::Create>,
+    ) -> Result<Json<response::Create>> {
+        struct Project {
+            id: i32,
+        }
+
+        let project = sqlx::query_as!(
+            Project,
+            "INSERT INTO projects (user_id, name, template, description) values ($1, $2, $3, $4) RETURNING id",
+            user_id as i32,
+            payload.name,
+            payload.template,
+            payload.description,
+        )
+        .fetch_one(&pool)
+        .await?;
+
+        Ok(Json(response::Create { id: project.id }))
     }
 
     pub async fn get(

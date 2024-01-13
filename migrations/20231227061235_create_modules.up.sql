ed1a641ac2d3ba0653b8a95ecf8732897c7784f7
Vladimir Zarypov <krre31@gmail.com>
2024-01-13 10:17:11 +0200

    Remove `created_at` field from `modules` table

--- migrations/20231227061235_create_modules.up.sql
+++ migrations/20231227061235_create_modules.up.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS modules (
     module_id int8 REFERENCES modules(id) ON DELETE CASCADE ON UPDATE CASCADE,
     name text NOT NULL,
     visibility smallint NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now()
 );
